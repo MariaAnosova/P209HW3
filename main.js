163c649500f06efe5d0d2840fc9d3533793fa4dc
maria Anosova <maria.anosova@bellevuecollege.edu>
2023-04-23 01:28:38 -0700

    Created the program for three buttons

---
+++ main.js
@@ -0,0 +1,29 @@
+let origArray = [2,3,4];
+document.getElementById("textbox1").value = origArray[0];
+document.getElementById("textbox2").value = origArray[1];
+document.getElementById("textbox3").value = origArray[2];
+
+function squareFunction() {
+    let squareResult = origArray.map(x => x ** 2);
+    document.getElementById("textbox1").value = squareResult[0];
+    document.getElementById("textbox2").value = squareResult[1];
+    document.getElementById("textbox3").value = squareResult[2];
+}
+
+let anonymousFunction = function() {
+    let cubeResult = origArray.map(x => x ** 3);
+    document.getElementById("textbox1").value = cubeResult[0];
+    document.getElementById("textbox2").value = cubeResult[1];
+    document.getElementById("textbox3").value = cubeResult[2];
+}
+document.querySelector("#button2").addEventListener("click", anonymousFunction);
+
+document.querySelector("#button3").addEventListener("click", getToFourth);
+function getToFourth() {
+    let toTheFourthResult = origArray.map(x => x ** 4);
+    document.getElementById("textbox1").value = toTheFourthResult[0];
+    document.getElementById("textbox2").value = toTheFourthResult[1];
+    document.getElementById("textbox3").value = toTheFourthResult[2];
+}
+
+
